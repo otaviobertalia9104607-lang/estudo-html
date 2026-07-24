747294e06d0e3eef686d448398437e870128ad90
otaviobertalia9104607-lang <otaviobertalia.9104607@edu.sme.prefeitura.sp.gov.br>
2026-07-24 18:53:01 -0300

    1

--- cinemática.js
+++ cinemática.js
@@ -1,1 +1,26 @@
+const estado ={
 
+    mes: new Date().getMounth(),
+    ano: new Date().getFullYear();
+};
+
+function proximoMes() {
+    if(estado.mes === 11){
+        //caso especial: altera o estado.ano e 0 o estado.mes
+        estado.mes = 0;
+        estado.ano = estado.ano + 1;
+    } else{
+        //caso normal: o estado.ano permanece o mesmo e soma 1 no estado.mes
+        estado.mes = estado.mes + 1;
+       }
+}
+function mesAnterior(){
+if(estado.mes === 0) {
+        //caso especial: subtrai 1 do estado.ano e altera o estado.mes para 11
+        estado.mes = 11;
+        estado.ano = estado.ano - 1;
+    } else{
+        //caso normal: o estado.ano permanece o mesmo e subtraí 1 no estado.mes
+        estado.mes = estado.mes - 1;
+       }
+}
