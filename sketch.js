b8e9da51f2e85cae3eab3bd33227066c39f58a21
otaviobertalia9104607-lang <otaviobertalia.9104607@edu.sme.prefeitura.sp.gov.br>
2026-07-25 13:22:03 -0300

    1

--- sketch.js
+++ sketch.js
@@ -28,4 +28,4 @@ if(estado.mes === 0) {
 cont somaMes = document.getElementById("somaMes");
 const subtraiMes = document.getElementById("subtraiMes");
 
-somaMes.addEventListener("click";proximoMes)
+somaMes.addEventListener("click",proximoMes)
